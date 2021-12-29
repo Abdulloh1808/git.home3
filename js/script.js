7ae3f65045b8845e7c8ee9fda7d5d48fd7f54cfd
abu <abu@gmail.com>
2021-12-29 16:17:46 +0500

    githome3

--- js/script.js
+++ js/script.js
@@ -1,12 +1,22 @@
-// let a = +prompt('luboy sonni kiriting')
-// while (isNaN(a) || a == 0) {
-//     a = +prompt('son kiriting son!')
-// }
-// if (a % 2==1) {
-//     alert('Siz toq son kiritdingiz '+a)
-// }else if (a % 2 == 0){
-//     alert('Siz juft son kiritdingiz '+a)
-// }
+let a = +prompt('luboy sonni kiriting')
+while (isNaN(a) || a == 0) {
+    a = +prompt('son kiriting son!')
+}
+if (a % 2==1) {
+    alert('Siz toq son kiritdingiz '+a)
+}else if (a % 2 == 0){
+    alert('Siz juft son kiritdingiz '+a)
+}
+
+
+let box = 1
+for (let i = 1 ; i < 6 ; i++) {
+    box = box * i
+    
+    console.log(box);
+}
+
+
 
 let son = +prompt('sonning qiymatini kiriting.')
 while (isNaN(son) || son == 0) {
@@ -16,10 +26,10 @@ let daraja = +prompt('sonning darajasini kiriting.')
 while (isNaN(daraja) || daraja == 0) {
     daraja = +prompt('Sonning darajasini bowqattan kiriting')
 }
-let box = son
-for (let i = son; i < daraja; ) {
-    box =box * son
-}alert('javob '+box)
+// let box = son
+// for (let i = son; i < daraja; ) {
+//     box =box * son
+// }alert('javob '+box)
 
 
 
@@ -30,10 +40,3 @@ for (let i = son; i < daraja; ) {
 
 
 
-
-// let box = 1
-// for (let i = 1 ; i < 6 ; i++) {
-//     box = box * i
-    
-//     console.log(box);
-// }
